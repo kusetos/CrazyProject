d29daa056407b45fa8587fc8425c92f476ebcf9c
kusetos <itokpak2004@gmail.com>
2024-02-12 23:59:31 +0600

    canvas

---
+++ Assets/_Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PalyGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void QuitGame()
+    {
+        Debug.Log("QuitGame");
+        Application.Quit();
+}

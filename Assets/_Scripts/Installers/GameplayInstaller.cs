96560d6d589408f8d54ea72e3e808aa4513bc1de
kusetos <itokpak2004@gmail.com>
2024-06-06 00:46:42 +0500

    commints

--- Assets/_Scripts/Installers/GameplayInstaller.cs
+++ Assets/_Scripts/Installers/GameplayInstaller.cs
@@ -9,10 +9,14 @@ public class GameplayInstaller : MonoInstaller
     [SerializeField] private Shooting player;
     public override void InstallBindings()
     {
+        //Bind Player
         Container.Bind<Shooting>().FromInstance(player).AsSingle();
+
+        // TIMER and SCORE in game
         Container.BindInterfacesAndSelfTo<GameTimer>().AsSingle();
         Container.Bind<GameScore>().AsSingle();
 
+        //Managers
         Container.Bind<UIGameManager>().FromComponentInHierarchy().AsSingle();
         Container.Bind<GameManager>().FromComponentInHierarchy().AsSingle();
     }
